ae658960a5b474abe98e6fed49e981356faeaaf4
Viggo Petersen <viggo.petersen@sallinggroup.com>
2018-10-22 14:54:27 +0200

    Add Quagga barCode reader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="item">i am a thing</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Quagga from 'quagga' // ES6
 
+/* eslint-disable */
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('#item')    // Or '#yourElement' (optional)
+      },
+      decoder : {
+        readers : ["code_128_reader"]
+      }
+  }, function(err) {
+      if (err) {
+        console.log(err);
+        return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+    });
   }
 }
 </script>
+
+<style>
+.home {
+  width: 100px;
+  height: 100px;
+  background-color: lightgray;
+}
+</style>
