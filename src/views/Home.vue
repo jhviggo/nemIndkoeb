fd8006f955e4ca9a92a472e291ea3e68085024ee
Viggo Petersen <viggo.petersen@sallinggroup.com>
2018-10-25 12:51:55 +0200

    testing scanner for mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="home">
-    <div id="item">i am a thing</div>
+  <div class="">
+    <div id="item"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
-import Quagga from 'quagga' // ES6
-
 /* eslint-disable */
+import HelloWorld from '@/components/HelloWorld.vue'
+import Quagga from 'quagga'
+
 export default {
   name: 'home',
   components: {
@@ -19,19 +19,73 @@ export default {
       inputStream : {
         name : "Live",
         type : "LiveStream",
-        target: document.querySelector('#item')    // Or '#yourElement' (optional)
+        target: document.querySelector('#item'),
+        constraints: {
+            width: 640,
+            height: 480
+        }
       },
       decoder : {
-        readers : ["code_128_reader"]
+          readers : ["ean_reader"]
+      }},
+        function(err) {
+          if (err) {
+            console.log(err);
+            return
+          }
+        Quagga.start();
+        console.log('Quagga has started!');
       }
-  }, function(err) {
-      if (err) {
-        console.log(err);
-        return
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
+    );
+
+      Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+          if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter(function (box) {
+              return box !== result.box;
+            }).forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+            });
+          }
+
+          if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+          }
+        }
+      });
+
+    Quagga.onDetected(result => {
+        if (result.codeResult){
+          document.getElementById('dd').innerHTML = result.codeResult.code;
+        }
     });
+  },
+  methods: {
+    scanImage(image) {
+        Quagga.decodeSingle({
+            decoder: {
+                readers: ["ean_reader"]
+            },
+            locate: true,
+            src: image
+        }, function(result){
+            console.log(result);
+            if(result.codeResult) {
+                console.log("result", result.codeResult.code);
+                document.getElementById('dd').innerHTML = result.codeResult.code;
+            } else {
+                console.log("not detected");
+            }
+        });
+    }
   }
 }
 </script>
